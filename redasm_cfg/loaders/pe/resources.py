860a3155ec7c62b351e878fa4e4523a85ba94041
Dax <contact@antoniodavide.dev>
2024-11-20 11:22:14 +0100

    PE: cleaned and improved PEResources class

--- redasm_cfg/loaders/pe/resources.py
+++ redasm_cfg/loaders/pe/resources.py
@@ -25,7 +25,9 @@ class PEResources:
     def __init__(self, pe, address):
         self.pe = pe
         self.rootaddress = address
+        self.entries = {}
         self.read_resource_dir(address)
+        print(self.entries)
 
     def read_resource_dir(self, address):
         resdir = redasm.set_type(address, "IMAGE_RESOURCE_DIRECTORY")
@@ -33,42 +35,50 @@ class PEResources:
         if c == 0:
             return
 
-        entries = redasm.set_type(address + redasm.size_of("IMAGE_RESOURCE_DIRECTORY"),
-                                  f"IMAGE_RESOURCE_DIRECTORY_ENTRY[{c}]")
-        for e in entries:
+        direntries = redasm.set_type(address + redasm.size_of("IMAGE_RESOURCE_DIRECTORY"),
+                                     f"IMAGE_RESOURCE_DIRECTORY_ENTRY[{c}]")
+        for e in direntries:
             if self.entry_nameisstring(e):
                 eaddr = self.rootaddress + self.entry_nameoffset(e)
                 namelen = redasm.set_type(eaddr, "u16")
-                redasm.set_type(eaddr + redasm.size_of("u16"), f"wchar[{namelen}]")
+                name = redasm.set_type(eaddr + redasm.size_of("u16"), f"wchar[{namelen}]")
+            else:
+                name = self.get_resource_id(e)
 
             if self.entry_isdirectory(e):
                 self.read_resource_dir(self.rootaddress + self.entry_directoryoffset(e))
             else:
-                data = redasm.set_type(self.rootaddress + e.Offset, "IMAGE_RESOURCE_DATA_ENTRY")
+                redasm.set_type(self.rootaddress + e.Offset, "IMAGE_RESOURCE_DATA_ENTRY")
+                # data = redasm.set_type(self.rootaddress + e.Offset, "IMAGE_RESOURCE_DATA_ENTRY")
+                # dataaddr = PE.rva_to_va(self.pe, data.OffsetToData)
+                # if dataaddr and data.Size:  # NOTE: Handle resource types?
+                # redasm.set_type(dataaddr, f"u8[{data.Size}]")
+
+            self.entries.setdefault(name, []).append(e)
+
+    def get_resource_data(self, entries):
+        res = []
+
+        for e in entries:
+            if not self.entry_isdirectory(e):
+                data = redasm.get_type(self.rootaddress + e.Offset, "IMAGE_RESOURCE_DATA_ENTRY")
                 dataaddr = PE.rva_to_va(self.pe, data.OffsetToData)
-                if dataaddr and data.Size:  # TODO: Handle resource types
-                    redasm.set_type(dataaddr, f"u8[{data.Size}]")
+                if dataaddr and data.Size:
+                    res.append((dataaddr, data.Size))
 
-    def find_id(self, id, parent=None):
-        if not parent:
-            parent = self.root
+        return res
 
-        return self.find_name(self.get_resource_id(id), parent)
-
-    def find_name(self, id, parent=None):
-        if not parent:
-            parent = self.root
-
-    def get_resource_id(self, id):
-        return PE_RESOURCE_TYPES.get(id, f"#{id}")
+    def get_resource_id(self, e):
+        resid = e.Id & ~0x80000000
+        return PE_RESOURCE_TYPES.get(resid, resid)
 
     def get_entry_name(self, e):
         if self.entry_nameisstring(e):
-            address = self._rsrcaddr + self.entry_nameoffset(e)
+            address = self.rootaddress + self.entry_nameoffset(e)
             namelen = redasm.set_type(address, "u16")
             return redasm.get_type(address + redasm.size_of("u16"), f"wchar[{namelen}]")
 
-        return self.get_resource_id(e.Id & ~1)
+        return self.get_resource_id(e.Id & ~0x80000000)
 
     def entry_isdirectory(self, e):
         return (e.Offset & 0x80000000) != 0
