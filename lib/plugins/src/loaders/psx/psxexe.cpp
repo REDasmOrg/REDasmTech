99f4e273a129e3ee9605de34f832fe2e26a4c786
Dax <contact@antoniodavide.dev>
2025-01-02 18:23:08 +0100

    feat(PSXEXE): read header from file buffer

--- lib/plugins/src/loaders/psx/psxexe.cpp
+++ lib/plugins/src/loaders/psx/psxexe.cpp
@@ -5,44 +5,43 @@ namespace psxexe {
 
 namespace {
 
-// constexpr int PSXEXE_SIGNATURE_SIZE = 8;
+constexpr int PSXEXE_SIGNATURE_SIZE = 8;
 constexpr std::string_view PSXEXE_SIGNATURE = "PS-X EXE";
 constexpr u32 PSXEXE_TEXT_OFFSET = 0x00000800;
 constexpr u32 PSX_USERRAM_START = 0x80000000;
 constexpr u32 PSX_USERRAM_END = 0x80200000;
 
-const RDStructField PSX_EXE_HEADER[] = {
-    {"char[8]", "id"},  {"u32", "text"},    {"u32", "data"},
-    {"u32", "pc0"},     {"u32", "gp0"},     {"u32", "t_addr"},
-    {"u32", "t_size"},  {"u32", "d_addr"},  {"u32", "d_size"},
-    {"u32", "b_addr"},  {"u32", "b_size"},  {"u32", "s_addr"},
-    {"u32", "s_size"},  {"u32", "SavedSP"}, {"u32", "SavedFP"},
-    {"u32", "SavedGP"}, {"u32", "SavedRA"}, {"u32", "SavedS0"},
-    {nullptr, nullptr},
+#pragma pack(push, 1)
+struct PsxExeHeader {
+    char id[PSXEXE_SIGNATURE_SIZE];
+    u32 text, data;
+    u32 pc0, gp0;
+    u32 t_addr, t_size;
+    u32 d_addr, d_size;
+    u32 b_addr, b_size;
+    u32 s_addr, s_size;
+    u32 SavedSP, SavedFP, SavedGP, SavedRA, SavedS0;
 };
+#pragma pack(pop)
 
 } // namespace
 
 bool init(RDLoader*) {
-    rd_createstruct("PSX_EXE_HEADER", PSX_EXE_HEADER);
-
     RDBuffer* file = rdbuffer_getfile();
-    RDValue* psxheader = rdvalue_create();
+    PsxExeHeader psxheader;
+    usize n = rdbuffer_read(file, 0, &psxheader, sizeof(PsxExeHeader));
 
-    if(!rdbuffer_collecttypename(file, 0, "PSX_EXE_HEADER", psxheader) ||
-       rdvalue_getstr(psxheader, "id", nullptr) != PSXEXE_SIGNATURE)
+    if(n != sizeof(PsxExeHeader) || psxheader.id != PSXEXE_SIGNATURE)
         return false;
 
     rd_setprocessor("mips32le");
     rd_map(PSX_USERRAM_START, PSX_USERRAM_END);
-    u32 taddr = rdvalue_getu32(psxheader, "t_addr", nullptr);
-    u32 tsize = rdvalue_getu32(psxheader, "t_size", nullptr);
-    u32 pc0 = rdvalue_getu32(psxheader, "pc0", nullptr);
 
-    rd_mapsegment_n("TEXT", taddr, tsize, PSXEXE_TEXT_OFFSET, tsize,
+    rd_mapsegment_n("TEXT", psxheader.t_addr, psxheader.t_size,
+                    PSXEXE_TEXT_OFFSET, psxheader.t_size,
                     SEG_HASCODE | SEG_HASDATA);
 
-    rd_setentry(pc0, "PSXEXE_EntryPoint");
+    rd_setentry(psxheader.pc0, "PSXEXE_EntryPoint");
     return true;
 }
 
