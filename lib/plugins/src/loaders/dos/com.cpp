938388bff4819bef107690bf9f288533d77672f0
Dax <contact@antoniodavide.dev>
2025-02-04 19:44:24 +0100

    feat: restored COM Loader

--- lib/plugins/src/loaders/dos/com.cpp
+++ lib/plugins/src/loaders/dos/com.cpp
@@ -8,17 +8,16 @@ using namespace std::string_view_literals;
 namespace {
 
 constexpr u16 COM_ENTRY = 0x0100;
-constexpr u16 MSEG_SIZE = 0xff00;
+constexpr u16 MSEG_SIZE = 0xffff;
 
 bool accept(const RDLoaderPlugin*, const RDLoaderRequest* req) {
-    return req->ext == "COM"sv && rdbuffer_getlength(req->file) <= MSEG_SIZE;
+    return req->ext == "COM"sv && rdbuffer_getlength(req->file) <= 0xff00;
 }
 
 bool load(RDLoader*, RDBuffer* file) {
     usize len = rdbuffer_getlength(file);
-
-    rd_map_n(0, 0x10000);
-    rd_mapsegment_n("MEM", COM_ENTRY, MSEG_SIZE, 0, len, SP_RWX);
+    rd_addsegment_n("MEM", 0, MSEG_SIZE, SP_RWX, 16);
+    rd_mapfile_n(0, COM_ENTRY, len);
     rd_setentry(COM_ENTRY, "_COM_EntryPoint_");
     return true;
 }
